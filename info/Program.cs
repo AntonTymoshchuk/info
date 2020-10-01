6f2cf17904cffd7aebc7915266d91e798184958f
Anton Tymoshchuk <aatymoshchuk@gmail.com>
2020-10-01 18:58:52 +0300

    Add project files.

---
+++ info/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Directory.GetLastWriteTime(@"C:\Users\Lenovo\Desktop\Folder"));
+        }
+    }
+}
